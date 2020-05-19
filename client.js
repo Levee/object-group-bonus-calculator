ba24740a2acb23c791ba0bbe4151a031bdfe7ffe
Levi <levicarlson123@gmail.com>
2020-05-19 16:48:54 -0500

    Co-authored-by: clevereda <clever.mukori@ndus.edu> Co-authored-by: cneisen234 <christopherjay71186@gmail.com> Co-authored-by: Blaine Booher <bgbooher@gmail.com>

--- client.js
+++ client.js
@@ -40,8 +40,11 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
-
+$(document).ready(whenReady);
+function whenReady() {
+  console.log('DOM is ready to be manipulated!');
+  $('#seeEmployees').on('click', printEmployees);
+} // end readyNow
 
 function calculateBonus(employeeObj){
   let bonusPercentage = 0;
@@ -92,42 +95,14 @@ function calculateBonus(employeeObj){
 }
 
 // loop over all employees and run the calculation for them
-for(let employeeObj of employees){
-  console.log(calculateBonus(employeeObj));
-  
+function printEmployees(){
+  let elObjects = $('#objects');
+  let newEmployee = ' ';
+  for(let employeeObj of employees){
+    newEmployee = JSON.stringify(calculateBonus(employeeObj));
+    elObjects.append(`<li>` + newEmployee + `</li>`);
+  }
 }
-
-
-l
-/*
-
-## Processing Employee Bonuses
-
-Loop over the `employees` array and do the following:
-
-* use each employee object as the input to the function described below.
-* `console.log` the results of each iteration.
-
-
-## Function Logic
-
-Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
-
-* The `name` property should contain the employee's name.
-* The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
-* The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
-* The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.*/
-
-
-// ### Individual Bonus calculation
-// - Those who have a rating of a 2 or below should not receive a bonus.
-// - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
-// - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
-// - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
-// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
-// and should receive an additional 5%.
-// - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
-// - No bonus can be above 13% or below 0% total.
 
 // NOTE: You may abstract out this bonus calculation into a second function if you like, but this is not mandatory.
 
